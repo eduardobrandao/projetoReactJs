b28a54944c0d3bce153aa5c55347af9c5f8f87e6
EduardoBrandao <eduardo.loureiro@madeinweb.app>
2023-03-09 23:39:07 -0300

    criação da sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { Header } from "./components/Header";
 import "./global.css";
 
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 export function App() {
   return (
     <div>
       <Header />
-      <h1>Hello Word !!!</h1>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h1>
+            Hello Word Hello WordHello WordHello WordHello WordHello WordHello
+            WordHello WordHello WordHello WordHello WordHello WordHello
+            WordHello WordHello WordHello WordHello WordHello WordHello
+            WordHello WordHello WordHello WordHello WordHello WordHello
+            WordHello WordHello WordHello WordHello WordHello WordHello
+            WordHello WordHello WordHello WordHello WordHello WordHello
+            WordHello WordHello WordHello WordHello WordHello WordHello Word !!!
+          </h1>
+        </main>
+      </div>
     </div>
   );
 }
